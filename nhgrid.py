b00c7647c6692195d3187f07f904876769b72e3d
Magnus Dahl <magnus@pepsi.imf.au.dk>
2014-06-05 16:16:19 +0200

    Function for generating 9 node network

---
+++ nhgrid.py
@@ -0,0 +1,25 @@
+import numpy as np
+import aurespf.solvers as au
+
+def nh_Nodes(admat=None, load_filename=None, full_load=False, alphas=None):
+
+    if alphas==None:
+        alphas = np.load("./results/nhOptimalMixes.npy")
+
+    if not admat:
+        admat = './settings/US_eurasia_openadmat.txt' # the whole world of connected
+                                        # superregions, as in Martins
+                                        # Frankfurt talk, plus the US connected
+                                        # to Europe with a link
+
+    regions = ['EU', 'RU', 'NA', 'ME', 'IN', 'SE', 'CN', 'JK', 'US']
+    files = [''.join([r,'.npz']) for r in regions]
+
+    prefix='VE_'
+    admat = admat
+
+    return au.Nodes(admat=admat, path='./data/', prefix=prefix,
+                    files=files, load_filename=load_filename,
+                    full_load=full_load, alphas=alphas,
+                    gammas=np.ones(9))
+
