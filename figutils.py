51830aa2a0d297e5f19d0e157c04db12b5601171
Magnus Dahl <magnus@pepsi.imf.au.dk>
2014-06-05 16:11:22 +0200

    First round of plotting, balancing vs transmission capacity, no impedance

--- figutils.py
+++ figutils.py
@@ -1,8 +1,199 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def BC_vs_TC(filenames = ['BC_TC_datacapM.npz', 'BC_TC_datacapR.npz'],\
-        path='./results/', labels = None, savepath='./results/figures/', figfilename = 'BC_vs_TC_RolMar.pdf', BC_type='both'):
+import aurespf.solvers as au
+from nhgrid import nh_Nodes
+from FCResult import FCResult
+from FlowCalculation import FlowCalculation
+
+#### Colorschemes ################################
+
+dth = (3.425)
+dcolwidth = (2*3.425+0.236)
+
+blue = '#134b7c'
+yellow = '#f8ca00'
+orange = '#e97f02'
+brown = '#876310'
+green = '#4a8e05'
+red = '#ae1215'
+purple = '#4f0a3d'
+darkred= '#4f1215'
+pink = '#bd157d'
+lightpink = '#d89bc2'
+aqua = '#37a688'
+darkblue = '#09233b'
+lightblue = '#8dc1e0'
+grayblue = '#4a7fa2'
+
+blue_cycle = [darkblue, blue, grayblue, lightblue]
+
+color_cycle = [blue, red, orange, purple, green, \
+               pink, lightblue, darkred, yellow]
+
+def ion_BE_vs_TC_testplot(layout):
+    """ This function plots the normalized balancing energy
+        as a function of transmission capacity for all the
+        regions in a provided layout. (sqr flow)
+
+        """
+
+    plt.close()
+    plt.ion()
+    plt.rcParams['axes.color_cycle'] = color_cycle
+    filenames = []
+    for a in np.linspace(0,1.5,31):
+        capacity = ''.join([str(a), 'q99'])
+        filenames.append(''.join([layout,'_aHE_',capacity,'_sqr.pkl']))
+
+    indexlist = get_indices_from_layout(layout)
+    N = nh_Nodes()
+    for i in indexlist:
+        TC = []
+        BE = []
+        for filename in filenames:
+            TC.append(get_data(filename, 'Total_TC',\
+                      './results/BalvsTransNoImpedance/')/1e6) # now in TW
+            BE.append(get_data(filename, 'BE',\
+                     './results/BalvsTransNoImpedance/')[i])
+
+        plt.plot(TC, BE, '-o', label=str(N[i].label) )
+    plt.xlabel('Total transmission capacity [TW]')
+    plt.ylabel('Balancing energy [normalized]')
+    plt.legend()
+
+
+def get_data(filename, field, path='./results/'):
+    """ Returns the data in a certain field,
+        from an FCResult file.
+        See the FCResult class for available fields.
+
+        Example
+        -------
+        >>> get_data("eurasia_aHE_0.95q99_lin.pkl", 'Total_TC')
+
+        """
+
+    result = FCResult(filename, path=path)
+    returnvalue = result.cache[0][field]
+
+    return returnvalue
+
+def make_all_noimp_BEBC_TC():
+    """ Running this generates the graphs
+        for BE vs TC and BC vs TC for all
+        layouts both for EU and for the whole layout together.
+
+        """
+
+    modes = ['lin', 'sqr']
+    layouts = ['EU_RU_NA_ME', 'eurasia', 'US_eurasia_open', \
+           'US_eurasia_closed', 'US_EU_RU_NA_ME']
+    ydatalabels = ['BE', 'BC']
+
+    regions = ['EU', 'all']
+
+    for r in regions:
+        for l in layouts:
+            fc_list = []
+            for m in modes:
+                fc = FlowCalculation(l, 'aHE', 'copper', m)
+                fc_list.append(fc)
+            for ydatalabel in ydatalabels:
+                savepath = ''.join(['./results/figures/', ydatalabel, 'vsTC/'])
+                make_bal_vs_trans_graph(fc_list, ydatalabel, region=r,\
+                                        savepath=savepath)
+
+
+def make_bal_vs_trans_graph(flowcalcs, ydatalabel, region='EU', \
+                    trans_scalerange=np.linspace(0,1.5, 31),\
+                    figfilename=None, savepath = './results/figures/', datapath='./results/BalvsTransNoImpedance/'):
+
+    """ Example
+        -------
+        make_bal_vs_trans_graph([flowcalclin, flowcalcsqr], 'BE')
+
+        """
+
+    plt.close()
+    plt.rc('lines', lw=2)
+
+    if not flowcalcs.__class__==list:
+        flowcalcs = [flowcalcs]
+
+    xmaxlist = []
+    for fc in flowcalcs:
+        filenames = []
+        for a in trans_scalerange:
+            capacity = ''.join([str(a), 'q99'])
+            filenames.append(''.join([str(FlowCalculation(fc.layout, fc.alphas, \
+                        capacity, fc.solvermode)), '.pkl']))
+
+        ydata = []
+        TC = []
+        if region=='EU':
+            for filename in filenames:
+                ydata.append(get_data(filename, ydatalabel, path=datapath)[0])
+                TC.append(get_data(filename, 'Total_TC', path=datapath)\
+                          /1e6) # now in TW
+
+        if region=='all':
+            N = nh_Nodes()
+            indexlist = get_indices_from_layout(fc.layout)
+            mean_load_list = [N[i].mean for i in xrange(9)]
+            ### the total mean load for the regions in the current layout
+            total_mean_load = np.sum([mean_load_list[i] for i in indexlist])
+
+            for filename in filenames:
+                unnormalized_data = [mean_load_list[i]*\
+                                     get_data(filename, ydatalabel, \
+                                     path=datapath)[i] for i in indexlist]
+                ydata.append(np.sum(unnormalized_data)/total_mean_load)
+                TC.append(get_data(filename, 'Total_TC', path=datapath)\
+                          /1e6) # now in TW
+
+
+        xmaxlist.append(np.max(TC))
+        plt.plot(TC,ydata, label=fc.solvermode)
+
+    plt.xlabel('Total transmission capacity [TW]')
+    if ydatalabel=='BE':
+        plt.ylabel('Balancing energy [normalized]')
+    if ydatalabel=='BC':
+        plt.ylabel('Balancing capacity [normalized]')
+
+    plt.legend()
+    plt.title(region + ': ' + flowcalcs[0].layout)
+    plt.xlim((0,np.min(xmaxlist)))
+    if not figfilename:
+        figfilename = ''.join([flowcalcs[0].layout, '_', ydatalabel, '_', \
+                region, '.pdf'])
+    plt.savefig(savepath+figfilename)
+    plt.close()
+
+def get_indices_from_layout(layout):
+    """ When given a layout, in the Nothern
+        hemisphere network, it returns a list
+        of indices of only the regions in said layuout.
+
+        """
+
+    indexlist = []
+    if 'EU_RU_NA_ME' in layout:
+        indexlist.extend(range(4))
+
+    if 'eurasia' in layout:
+        indexlist.extend(range(8))
+
+    if 'US' in layout:
+        indexlist.append(8)
+
+    return indexlist
+
+
+def BC_vs_TC_old(filenames = ['BC_TC_datacapM.npz', 'BC_TC_datacapR.npz'],\
+        path='./results/', labels = None, savepath='./results/figures/',\
+        figfilename = 'BC_vs_TC_RolMar.pdf', BC_type='both'):
     plt.close()
     plt.rc('lines', lw=2)
 
